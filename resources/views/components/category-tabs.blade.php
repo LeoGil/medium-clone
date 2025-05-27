0bfb2e0fd71b98ce89dd9fa55000ca987549e666
Leonardo Gil <leogilc@gmail.com>
2025-05-27 19:39:22 -0300

    componentizando os posts e as categorias

---
+++ resources/views/components/category-tabs.blade.php
@@ -0,0 +1,14 @@
+<div class="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
+    <ul class="flex justify-center flex-wrap -mb-px">
+        <li class="me-2">
+            <a href="#" class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300">All</a>
+        </li>
+        @foreach ($categories as $category)
+            <li class="me-2">
+                <a href="#" class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300">
+                    {{ $category->name }}
+                </a>
+            </li>
+        @endforeach
+    </ul>
+</div>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,23 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <div class="py-4">
+        <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                <div class="p-4 text-gray-900">
+                    <x-category-tabs />
                 </div>
             </div>
+            <div class="mt-8 text-gray-900">
+                @forelse ($posts as $post)
+                    <x-post-item :post="$post" />
+                @empty
+                    <div class="flex bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 mb-8">
+                        <div class="p-5 flex-1">
+                            <h5 class="mb-2 text-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white">No posts found</h5>
+                        </div>
+                    </div>
+                @endforelse
+            </div>
+            {{ $posts->links() }}
         </div>
     </div>
 </x-app-layout>
