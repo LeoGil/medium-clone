0cd42afed06d32a451ef7377a08937fb576ea2b4
Leonardo Gil <leogilc@gmail.com>
2025-06-03 00:33:51 -0300

    bugfix: ajustando css para nao quebrar a imagem do usuario quando nao for quadrada

--- resources/views/components/user-avatar.blade.php
+++ resources/views/components/user-avatar.blade.php
@@ -1,7 +1,11 @@
 @props(['user', 'w' => 12, 'h' => 12])
 
+@php
+    $classes = "w-{$w} h-{$h} object-cover rounded-full";
+@endphp
+
 @if ($user->image)
-    <img src="{{ $user->imageUrl() }}" alt="{{ $user->name }}" class="w-{{ $w }} h-{{ $h }} rounded-full">
+    <img src="{{ $user->imageUrl() }}" alt="{{ $user->name }}" class="{{ $classes }}">
 @else
-    <img src="https://ui-avatars.com/api/?name={{ $user->name }}" alt="{{ $user->name }}" class="w-{{ $w }} h-{{ $h }} rounded-full">
+    <img src="https://ui-avatars.com/api/?name={{ $user->name }}" alt="{{ $user->name }}" class="{{ $classes }}">
 @endif
